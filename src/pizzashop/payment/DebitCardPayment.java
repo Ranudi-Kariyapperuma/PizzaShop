ff8d57432455faf001355d6b2a8392ed6ece9a36
Ranudi Gayathmie Kariyapperuma <ranudigk@gmail.com>
2024-12-29 01:51:42 +0530

    changes

--- src/pizzashop/payment/DebitCardPayment.java
+++ src/pizzashop/payment/DebitCardPayment.java
@@ -4,19 +4,19 @@
  */
 package pizzashop.payment;
 
-// DebitCardPayment implements PaymentStrategy interface
+
 public class DebitCardPayment implements PaymentStrategy {
 
     @Override
     public void pay(double amount) {
-        // Print a message indicating Debit Card payment processing
+        
         System.out.println("Processing Debit Card payment of $" + amount);
 
-        // Simulate Debit Card payment logic here, like connecting to a banking API
+    
         try {
-            // Simulating payment transaction
+            
             System.out.println("Connecting to bank...");
-            Thread.sleep(1000); // Simulate delay in processing payment
+            Thread.sleep(1000); 
             System.out.println("Payment successful via Debit Card!");
         } catch (InterruptedException e) {
             System.out.println("Payment process was interrupted.");
